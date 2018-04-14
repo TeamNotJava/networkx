346c8ac1c1e55a0c6f553ce73a017a9e816a91a3
Yassin Bahloul <yassin.bahloul@rwth-aachen.de>
2018-04-14 21:57:43 +0200

    add min_fill_in_heuristic_faster: sort nodes by degree and prune early.

--- networkx/algorithms/approximation/treewidth.py
+++ networkx/algorithms/approximation/treewidth.py
@@ -37,6 +37,49 @@ def min_degree_heuristic(G):
 
 
 # Returns the node with minimum degree or None (if the abort condition is met)
+# sort nodes according to degree and prune early while iterating neighbor pairs
+def min_fill_in_heuristic_faster(G):
+    if len(G) == 0:
+        return None
+
+    candidate_node = None
+
+    min_fill_in = sys.maxsize
+
+    # create sorted list of (degree, node)
+    degree_list = [(degree, node) for (node, degree) in G.degree]
+    degree_list.sort()
+
+    # abort condition
+    min_degree = degree_list[0][0]
+    if min_degree == len(G) - 1:
+        return None
+
+    for (degree, node) in degree_list:
+        min_degree = min(min_degree, degree)
+        num_fill_in = 0
+        # Convert to list in order to access by index
+        neighbors = list(G.neighbors(node))
+        for i in range(len(neighbors) - 1):
+            for j in range(i + 1, len(neighbors)):
+                if not G.has_edge(neighbors[i], neighbors[j]):
+                    num_fill_in += 1
+                    # prune if this can't be min-fill-in node anymore
+                    if num_fill_in >= min_fill_in:
+                        break
+            else:
+                continue # executed if no break
+            break
+
+        if num_fill_in < min_fill_in:
+            if num_fill_in == 0:
+                return node
+            min_fill_in = num_fill_in
+            candidate_node = node
+
+    return candidate_node
+
+# Returns the node with minimum degree or None (if the abort condition is met)
 def min_fill_in_heuristic(G):
     candidate_node = None
     # Still keep track of min_degree to abort earlier
