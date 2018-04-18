e188a6b9997769989ceb85b35b60792125cbc6b2
Valkum <rudi.floren@googlemail.com>
2018-04-18 18:27:21 +0200

    Improve Treewidth tests.
    Add class for min_fill_hin heuristic.

--- networkx/algorithms/approximation/tests/test_treewidth.py
+++ networkx/algorithms/approximation/tests/test_treewidth.py
@@ -1,10 +1,22 @@
+#-*- coding: utf-8 -*-
+#    Copyright (C) 2018 by
+#    Rudolf-Andreas Floren <rudi.floren@gmail.com>
+#    Dominik Meier <dominik.meier@rwth-aachen.de>
+#    All rights reserved.
+#    BSD license.
 import networkx as nx
 from nose.tools import assert_equals
 from nose.tools import ok_
 from networkx.algorithms.approximation import treewidth_min_degree
+from networkx.algorithms.approximation import treewidth_min_fill_in
+"""Unit tests for the :mod:`networkx.algorithms.approximation.treewidth`
+module.
+
+"""
 
 
 def is_tree_decomp(graph, decomp):
+    """Check if the given decomposition tree is a decomposition tree of the given graph"""
     isTreeDecomp = True
     for x in graph.nodes():
         appearOnce = False
@@ -21,7 +33,7 @@ def is_tree_decomp(graph, decomp):
                 appearTogether = True
         ok_(appearTogether)
 
-        # Check if the nodes associated with vertex v form a connected subset of T
+    # Check if the nodes associated with vertex v form a connected subset of T
     for v in graph.nodes():
         subset = []
         for bag in decomp.nodes():
@@ -31,23 +43,87 @@ def is_tree_decomp(graph, decomp):
         ok_(nx.is_connected(subGraph))
 
 
-class TestTreewidth(object):
+class TestTreewidthMinDegree(object):
+    """Unit tests for the :mod:`networkx.algorithms.approximation.treewidth_min_degree`
+    function.
+
+    """
+
+    def setUp(self):
+        """Setup for different kinds of trees"""
+        self.smallTree = nx.Graph()
+        self.smallTree.add_edge(1, 3)
+        self.smallTree.add_edge(4, 3)
+        self.smallTree.add_edge(2, 3)
+        self.smallTree.add_edge(3, 5)
+        self.smallTree.add_edge(5, 6)
+        self.smallTree.add_edge(5, 7)
+        self.smallTree.add_edge(6, 7)
+
     def test_tree_decomposition(self):
-        # Test if the returned decomposition is a valid decomposition for a selection of various graphs
+        """Test if the returned decomposition is a valid decomposition for a selection 
+        of various graphs
+        """
         G = nx.petersen_graph()
-        treewidth, decomp = treewidth_min_degree(G)
+        _, decomp = treewidth_min_degree(G)
         is_tree_decomp(G, decomp)
         # Check if each vertex appears at least once in a bag
 
-    def test_min_degreee(self):
-        G = nx.Graph()
+    def test_small_tree_treewidth(self):
+        """Test if the computed treewidth of the known self.smallTree is 2.
+        As we know which value we can expect from our heuristic, values other than two are regressions
+
+        """
+        G = self.smallTree
         # The order of removal should be (with [] denoting any order of the containing nodes) [1,2,4]3[5,6,7], resulting in treewdith 2 for the heuristic
-        G.add_edge(1, 3)
-        G.add_edge(4, 3)
-        G.add_edge(2, 3)
-        G.add_edge(3, 5)
-        G.add_edge(5, 6)
-        G.add_edge(5, 7)
-        G.add_edge(6, 7)
-        treewidth, decomp = treewidth_min_degree(G)
+        
+        treewidth, _ = treewidth_min_fill_in(G)
         assert_equals(treewidth, 2)
+
+
+    def test_mid_tree_treewidth(self):
+        pass
+
+    def test_large_tree_treewidth(self):
+        pass
+
+
+class TestTreewidthMinFillIn(object):
+    """Unit tests for the :mod:`networkx.algorithms.approximation.treewidth_min_fill_in`
+    function.
+
+    """
+
+    def setUp(self):
+        """Setup for different kinds of trees"""
+        self.smallTree = nx.Graph()
+        self.smallTree.add_edge(1, 3)
+        self.smallTree.add_edge(4, 3)
+        self.smallTree.add_edge(2, 3)
+        self.smallTree.add_edge(3, 5)
+        self.smallTree.add_edge(5, 6)
+        self.smallTree.add_edge(5, 7)
+        self.smallTree.add_edge(6, 7)
+
+    def test_tree_decomposition(self):
+        """Test if the returned decomposition is a valid decomposition for a selection 
+        of various graphs
+        """
+        G = nx.petersen_graph()
+        _, decomp = treewidth_min_fill_in(G)
+        is_tree_decomp(G, decomp)
+        # Check if each vertex appears at least once in a bag
+
+    def test_small_tree_treewidth(self):
+        """Test if the computed treewidth of the known self.smallTree is 2"""
+        G = self.smallTree
+        # The order of removal should be (with [] denoting any order of the containing nodes) [1,2,4]3[5,6,7], resulting in treewdith 2 for the heuristic
+        
+        treewidth, _ = treewidth_min_degree(G)
+        assert_equals(treewidth, 2)
+
+    def test_mid_tree_treewidth(self):
+        pass
+
+    def test_large_tree_treewidth(self):
+        pass
