b6b41197b2f9afbb3b5947b7a83e7d0e490bec69
Petrov <petrov.pepi@yahoo.com>
2018-04-18 19:39:47 +0200

    Merge branch 'treewidth-min-deg-heap' of C:\Users\petre\PycharmProjects\networkx with conflicts.

--- networkx/algorithms/approximation/treewidth.py
+++ networkx/algorithms/approximation/treewidth.py
@@ -19,24 +19,119 @@ def treewidth_min_fill_in(G):
     return treewidth_decomp(G, min_fill_in_heuristic)
 
 
-# Returns the node that needs to be removed next or None (if the abort condition is met)
-def min_degree_heuristic(G):
+def min_degree_node_heuristic(G: Graph):
+    """Returns the node from the graph with minimum degree.
+
+        Parameters
+        ----------
+        G : Graph
+
+        Returns
+        -------
+        min_degree_node : string, integers or hashable Python object (except None)
+            The node from the graph with lowest degree. If the graph is fully connected
+            then the function returns None.
+
+        Notes
+        -----
+        This algorithm computes the node with lowest number of edges in the graph 'G'.
+        The running time of the algorithm is O(V) and it uses constant
+        additional memory.
+
+        References
+        ----------
+        .. [1] K. Wang, Z. Lu, and J. Hicks
+               *Treewidth*.
+               http://web.eecs.utk.edu/~cphillip/cs594_spring2015_projects/treewidth.pdf
+
+        """
+
     min_degree = sys.maxsize
-    min_node = None
+    min_degree_node = None
     for (node, degree) in G.degree:
         if degree < min_degree:
             if degree <= 1:
                 # Return early
                 return node
-            min_node = node
+            min_degree_node = node
             min_degree = degree
 
     if min_degree == G.number_of_nodes() - 1:
         # Fully connected: Abort condition
         return None
     else:
-        return min_node
+        return min_degree_node
 
+
+
+def min_fill_in_heuristic_faster(G):
+    """Returns the node from the graph, where the number of edges added  when
+    turning the neighbourhood of the chosen node into clique is small as possible.
+
+            Parameters
+            ----------
+            G : Graph
+
+            Returns
+            -------
+            min_fill_node : string, integers or hashable Python object (except None)
+                The node from the graph, for which, when it is deleted from the graph and
+                its neighbourhood is turned into clique, the number of edges added is
+                small as possible.
+
+            Notes
+            -----
+            This algorithm computes the node with 'min fill in' in the graph 'G'.
+            The running time of the algorithm is O(V*V*V) and it uses constant
+            additional memory.
+
+            References
+            ----------
+            .. [1] K. Wang, Z. Lu, and J. Hicks
+                   *Treewidth*.
+                   http://web.eecs.utk.edu/~cphillip/cs594_spring2015_projects/treewidth.pdf
+
+            """
+
+    if len(G) == 0:
+        return None
+
+    min_fill_in_node = None
+
+    min_fill_in = sys.maxsize
+
+    # create sorted list of (degree, node)
+    degree_list = [(degree, node) for (node, degree) in G.degree]
+    degree_list.sort()
+
+    # abort condition
+    min_degree = degree_list[0][0]
+    if min_degree == len(G) - 1:
+        return None
+
+    for (degree, node) in degree_list:
+        min_degree = min(min_degree, degree)
+        num_fill_in = 0
+        # Convert to list in order to access by index
+        neighbors = list(G.neighbors(node))
+        for i in range(len(neighbors) - 1):
+            for j in range(i + 1, len(neighbors)):
+                if not G.has_edge(neighbors[i], neighbors[j]):
+                    num_fill_in += 1
+                    # prune if this can't be min-fill-in node anymore
+                    if num_fill_in >= min_fill_in:
+                        break
+            else:
+                continue # executed if no break
+            break
+
+        if num_fill_in < min_fill_in:
+            if num_fill_in == 0:
+                return node
+            min_fill_in = num_fill_in
+            min_fill_in_node = node
+
+    return min_fill_in_node
 
 # Returns the node with minimum degree or None (if the abort condition is met)
 def min_fill_in_heuristic(G):
@@ -71,6 +166,7 @@ def min_fill_in_heuristic(G):
 # Calculates tree width decomposition using the passed heuristic
 # Returns tuple: (treewidth: int, decomposition: Graph)
 def treewidth_decomp(G, heuristic):
+    # copy so the original graph is not modified
     G = G.copy()
 
     # stack where nodes and their neighbors are pushed in the order they are selected by the heuristic
@@ -82,7 +178,7 @@ def treewidth_decomp(G, heuristic):
         neighbors = set(G.neighbors(elim_node))
         for n in neighbors:
             for m in neighbors:
-                if (n != m) and not G.has_edge(n, m):
+                if n != m and not G.has_edge(n, m):
                     G.add_edge(n, m)
 
         # remove node from graph and push on stack (including its neighbors)
@@ -119,6 +215,7 @@ def treewidth_decomp(G, heuristic):
 
         # Add edge to decomposition (implicitly also adds the new node)
         decomp.add_edge(old_bag, neighbors)
+
     return current_treewidth, decomp
 
 # Calculates tree width decomposition according to the minimum degree heuristic
@@ -197,10 +294,8 @@ def treewidth_decomp_min_degree(G):
     return current_treewidth, decomp
 
 
-# Calculates tree width decomposition according to the minimum degree heuristic
-# Returns tuple: (treewidth: int, decomposition: Graph)
-def treewidth_decomp_min_degree_faster(G):
-    #G = G.copy()
+def treewidth_decomp_min_fill_in_heap_implementation(G):
+    G = G.copy()
     graph = {}
     for u in G:
         graph[u] = set()
@@ -219,15 +314,24 @@ def treewidth_decomp_min_degree_faster(G):
     # build heap with initial degrees
     #for (n, degree) in G.degree:
     #    degreeq.append((degree, n))
+    min_fill_in_state = {}
     for n in graph:
-        degreeq.append((len(graph[n]), n))
+        neighbors = graph[n]
+        current_fill_in = 0
+        for u in neighbors:
+            for v in neighbors:
+                if u != v and u not in graph[v]:
+                    current_fill_in += 1
+        degreeq.append((current_fill_in, n))
+        min_fill_in_state[n] = current_fill_in
     heapify(degreeq)
+
 
     while degreeq:
         # get the next (minimum degree) node
         (min_degree, elim_node) = pop(degreeq)
         #if not G.has_node(elim_node) or G.degree[elim_node] != min_degree:
-        if not elim_node in graph or len(graph[elim_node]) != min_degree:
+        if not elim_node in graph or min_fill_in_state[elim_node] != min_degree:
             # Outdated entry in degreeq
             continue
         #elif min_degree == G.number_of_nodes() - 1:
@@ -246,8 +350,6 @@ def treewidth_decomp_min_degree_faster(G):
                     #G.add_edge(n, m)
                     graph[n].add(m)
                     graph[m].add(n)
-                    changed_degree.add(n)
-                    changed_degree.add(m)
 
         # remove node from graph and push on stack (including its neighbors)
         #G.remove_node(elim_node)
@@ -259,8 +361,15 @@ def treewidth_decomp_min_degree_faster(G):
 
         # insert changed degrees into degreeq
         for n in changed_degree:
-            #push(degreeq, (G.degree[n], n))
-            push(degreeq, (len(graph[n]), n))
+            changed_node_neighbours = graph[n]
+            changed_node_min_fill_in = 0
+            for u in changed_node_neighbours:
+                for v in changed_node_neighbours:
+                    if u != v and u not in graph[v]:
+                        changed_node_min_fill_in += 1
+
+            push(degreeq, (changed_node_min_fill_in, n))
+            min_fill_in_state[n] = changed_node_min_fill_in
 
     # The abort condition is met. Put all nodes into one bag.
     decomp = nx.Graph()
@@ -291,28 +400,3 @@ def treewidth_decomp_min_degree_faster(G):
         decomp.add_edge(old_bag, neighbors)
 
     return current_treewidth, decomp
-
-if __name__ == '__main__':
-    # Test on graph from page 2 of "Discovering Treewidth" (Hans L. Bodlaender)
-    """
-    G = nx.Graph()
-    G.add_edges_from([('a', 'b'), ('b', 'c'), ('b', 'd'),
-                      ('c', 'e'), ('c', 'f'), ('d', 'f'),
-                      ('d', 'g'), ('e', 'f'), ('f', 'g')])
-    """
-    G = nx.fast_gnp_random_graph(2000, 0.01, directed=False)
-
-    start = time.time()
-    (tw, twdecomp) = treewidth_decomp_min_degree_faster(G)
-    end = time.time()
-    print tw, end - start
-
-    start = time.time()
-    (tw, twdecomp) = treewidth_decomp_min_degree(G)
-    end = time.time()
-    print tw, end - start
-
-    start = time.time()
-    (tw, twdecomp) = treewidth_decomp(G,min_degree_heuristic)
-    end = time.time()
-    print tw, end - start
