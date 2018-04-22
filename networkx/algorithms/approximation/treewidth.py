058f7703c44352086f705a143758e430915d1231
Johannes Schulte <johannesb.schulte@gmail.com>
2018-04-22 20:46:15 +0200

    Implemented heuristic iterator

--- networkx/algorithms/approximation/treewidth.py
+++ networkx/algorithms/approximation/treewidth.py
@@ -15,6 +15,7 @@
 import sys
 
 import networkx as nx
+from heapq import heappush, heappop, heapify
 
 __all__ = ["treewidth_min_degree", "treewidth_min_fill_in"]
 
@@ -34,7 +35,7 @@ def treewidth_min_degree(G):
     Treewidth decomposition : (int, Graph) tuple
           2-tuple with treewidth and the corresponding decomposed tree (NetworkX graph).
     """
-    return treewidth_decomp(G, min_degree_heuristic)
+    return treewidth_decomp(G, MinDegreeHeuristic)
 
 
 # Returns a tuple: (treewidth: int, decomposition: Graph)
@@ -50,149 +51,208 @@ def treewidth_min_fill_in(G):
     Treewidth decomposition : (int, Graph) tuple
         2-tuple with treewidth and the corresponding decomposed tree (NetworkX graph).
     """
-    return treewidth_decomp(G, min_fill_in_heuristic)
+    return treewidth_decomp(G, MinFillInHeuristic)
 
 
-# Returns the node that needs to be removed next or None (if the abort condition is met)
-def min_degree_heuristic(G):
-    """ Returns the node that needs to be removed next or None according to the Minimum Degree heuristic that selects the vertex with the minimum number of unselected neighbours.
+class MinDegreeHeuristic:
+    # TODO: Update documentation
+    def __init__(self, graph):
+        self._graph = graph
 
+        # A collection of nodes that have to be updated in the heap before each iteration
+        self._update_nodes = []
+
+        # self._degreeq is heapq with 2-tuples (degree,node)
+        self._degreeq = []
+        # build heap with initial degrees
+        for n in graph:
+            self._degreeq.append((len(graph[n]), n))
+        heapify(self._degreeq)
+
+    def __iter__(self):
+        return self
+
+    # Implement next method for backwards compatibility with python 2
+    def next(self):
+        return self.__next__()
+
+    def __next__(self):
+        # Update nodes in self._update_nodes
+        for n in self._update_nodes:
+            # insert changed degrees into degreeq
+            heappush(self._degreeq, (len(self._graph[n]), n))
+
+        while self._degreeq:
+            # get the next (minimum degree) node
+            (min_degree, elim_node) = heappop(self._degreeq)
+            if not elim_node in self._graph or len(self._graph[elim_node]) != min_degree:
+                # Outdated entry in degreeq
+                continue
+            elif min_degree == len(self._graph) - 1:
+                # Fully connected: Abort condition
+                raise StopIteration
+
+            # Remember to update nodes in the heap before getting the next node
+            self._update_nodes = self._graph[elim_node]
+            return elim_node
+
+        # The heap is empty: Abort
+        raise StopIteration
+
+
+class MinFillInHeuristic:
+    """Returns the node from the graph, where the number of edges added  when
+    turning the neighbourhood of the chosen node into clique is small as possible.
     Parameters
     ----------
-    G : NetworkX graph
-
+    G : Graph
     Returns
     -------
-    Node : NetworkX graph
-         
+    min_fill_node : string, integers or hashable Python object (except None)
+        The node from the graph, for which, when it is deleted from the graph and
+        its neighbourhood is turned into clique, the number of edges added is
+        small as possible.
+    Notes
+    -----
+    This algorithm computes the node with 'min fill in' in the graph 'G'.
+    The running time of the algorithm is O(V*V*V) and it uses constant
+    additional memory.
     References
     ----------
-    .. [2] Hand L. Bodlaender. "Discovering Treewidth". institute of information and computing sciences, utrecht university. technical report UU-CS-2005-018. www.cs.uu.nl
+    .. [1] K. Wang, Z. Lu, and J. Hicks
+           *Treewidth*.
+           http://web.eecs.utk.edu/~cphillip/cs594_spring2015_projects/treewidth.pdf
     """
-    min_degree = sys.maxsize
-    min_node = None
-    for (node, degree) in G.degree:
-        if degree < min_degree:
-            if degree <= 1:
-                # Return early
-                return node
-            min_node = node
-            min_degree = degree
+    #TODO: Update documentation
+    def __init__(self, graph):
+        self._graph = graph
 
-    if min_degree == G.number_of_nodes() - 1:
-        # Fully connected: Abort condition
-        return None
-    else:
-        return min_node
+    def __iter__(self):
+        return self
 
+    # Implement next method for backwards compatibility with python 2
+    def next(self):
+        return self.__next__()
 
-# Returns the node with minimum degree or None (if the abort condition is met)
-def min_fill_in_heuristic(G):
-    """Returns the node with minimum degree or None according to the Minimum Fill-in heuristic that selects a vertex which geves the minimum number of added fill-in edges for the current step.
+    def __next__(self):
+        if len(self._graph) == 0:
+            raise StopIteration
 
-    Parameters
-    ----------
-    G : NetworkX graph
+        min_fill_in_node = None
 
-    Returns
-    -------
-    Node : NetworkX graph
+        min_fill_in = sys.maxsize
 
-    References
-    ----------
-    .. [2] Hand L. Bodlaender. "Discovering Treewidth". institute of information and computing sciences, utrecht university. technical report UU-CS-2005-018. www.cs.uu.nl
-    """
-    candidate_node = None
-    # Still keep track of min_degree to abort earlier
-    min_degree = sys.maxsize
-    min_fill_in = sys.maxsize
+        # create sorted list of (degree, node)
+        degree_list = [(len(self._graph[node]), node) for node in self._graph]
+        degree_list.sort()
 
-    for (node, degree) in G.degree:
-        min_degree = min(min_degree, degree)
-        num_fill_in = 0
-        # Convert to list in order to access by index
-        neighbors = list(G.neighbors(node))
-        for i in range(len(neighbors) - 1):
-            for j in range(i + 1, len(neighbors)):
-                if not G.has_edge(neighbors[i], neighbors[j]):
-                    num_fill_in += 1
+        # abort condition
+        min_degree = degree_list[0][0]
+        if min_degree == len(self._graph) - 1:
+            raise StopIteration
 
-        if num_fill_in < min_fill_in:
-            if num_fill_in == 0:
-                return node
-            min_fill_in = num_fill_in
-            candidate_node = node
+        for (degree, node) in degree_list:
+            min_degree = min(min_degree, degree)
+            num_fill_in = 0
+            # Convert to list in order to access by index
+            neighbors = list(self._graph[node])
+            for i in range(len(neighbors) - 1):
+                for j in range(i + 1, len(neighbors)):
+                    if not neighbors[j] in self._graph[neighbors[i]]:
+                        num_fill_in += 1
+                        # prune if this can't be min-fill-in node anymore
+                        if num_fill_in >= min_fill_in:
+                            break
+                else:
+                    continue  # executed if no break
+                break
 
-    if min_degree == G.number_of_nodes() - 1:
-        # Fully connected: Abort condition
-        return None
-    else:
-        return candidate_node
+            if num_fill_in < min_fill_in:
+                if num_fill_in == 0:
+                    return node
+                min_fill_in = num_fill_in
+                min_fill_in_node = node
+
+        return min_fill_in_node
 
 
 # Calculates tree width decomposition using the passed heuristic
 # Returns tuple: (treewidth: int, decomposition: Graph)
-def treewidth_decomp(G, heuristic):
+def treewidth_decomp(G, heuristic_class):
     """Returns a treewidth decomposition using the passed heuristic.
 
     Parameters
     ----------
     G : NetworkX graph
-    heuristic : function
+    heuristic_class : iterator class
 
     Returns
     -------
     Treewidth decomposition : (int, Graph) tuple
         2-tuple with treewidth and the corresponding decomposed tree (NetworkX graph).
     """
-    # copy so the original graph is not modified
-    G = G.copy()
+    # TODO: Update documentation regarding heuristic_class
+    # make dict-of-sets structure
+    graph = {}
+    for u in G:
+        graph[u] = set()
+        for v in G[u]:
+            if u != v:  # ignore self-loop
+                graph[u].add(v)
 
     # stack where nodes and their neighbors are pushed in the order they are selected by the heuristic
     node_stack = []
 
-    elim_node = heuristic(G)
-    while elim_node is not None:
+    # instanciate a heuristic_iterator
+    heuristic_iterator = heuristic_class(graph)
 
+    for elim_node in heuristic_iterator:
         # Connect all neighbours with each other
-        neighbors = set(G.neighbors(elim_node))
+        neighbors = graph[elim_node]
         for n in neighbors:
             for m in neighbors:
-                if n != m and not G.has_edge(n, m):
-                    G.add_edge(n, m)
+                if n != m and not m in graph[n]:
+                    graph[n].add(m)
+                    graph[m].add(n)
 
-        # remove node from graph and push on stack (including its neighbors)
-        G.remove_node(elim_node)
+        # push node and its current neighbors on stack
         node_stack.append((elim_node, neighbors))
 
-        # get next node to be removed according to heuristic
-        elim_node = heuristic(G)
+        # remove node from graph
+        for u in graph:
+            if elim_node in graph[u]:
+                graph[u].remove(elim_node)
+        del graph[elim_node]
 
     # The abort condition is met. Put all nodes into one bag.
     decomp = nx.Graph()
-    new_bag = frozenset(G.nodes)
-    decomp.add_node(new_bag)
+    first_bag = frozenset(graph.keys())
+    decomp.add_node(first_bag)
 
-    current_treewidth = len(new_bag) - 1
+    treewidth = len(first_bag) - 1
 
     while node_stack:
         # get node and its neighbors from the stack
         (curr_node, neighbors) = node_stack.pop()
+
         # find a bag the neighbors are in
         old_bag = None
         for bag in decomp.nodes:
             if neighbors <= bag:
                 old_bag = bag
                 break
+        if old_bag == None:
+            # no old_bag was found: just connect to the first_bag
+            old_bag = first_bag
 
         # Create new node for decomposition
         neighbors.add(curr_node)
-        neighbors = frozenset(neighbors)
+        new_bag = frozenset(neighbors)
 
         # Update treewidth
-        current_treewidth = max(current_treewidth, len(neighbors)-1)
+        treewidth = max(treewidth, len(new_bag) - 1)
 
         # Add edge to decomposition (implicitly also adds the new node)
-        decomp.add_edge(old_bag, neighbors)
+        decomp.add_edge(old_bag, new_bag)
 
-    return current_treewidth, decomp
+    return treewidth, decomp
