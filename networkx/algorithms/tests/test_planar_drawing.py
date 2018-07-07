9cd147b330ad1d8ca221d647593f68ef752d4aba
Dominik Meier <dominik.meier@rwth-aachen.de>
2018-07-07 13:29:03 +0200

    Added test for intersections

--- networkx/algorithms/tests/test_planar_drawing.py
+++ networkx/algorithms/tests/test_planar_drawing.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import math
 from nose.tools import assert_true
 
 
@@ -27,7 +28,26 @@ def is_planar_drawing_correct(G, pos):
     -------
     is_correct : bool
     """
-    pass
+    for (a,b) in G.edges():
+        for (c,d) in G.edges:
+            if(a != c and b != d): #need to have different end points for a chance of conflict
+                x1,y1 = pos[a]
+                x2,y2 = pos[b]
+                x3,y3 = pos[c]
+                x4,y4 = pos[d]
+                determinant = (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)
+                if(determinant != 0): #the lines are not parallel
+                    #calculate intersection point, see https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+                    px = (x1*y2 - y1*x2)*(x3 - x4) - (x1-x2)*(x3*y4-y3*x4) /determinant
+                    py = (x1*y2 - y1*x2)*(y3 - y4) - (y1-y2)*(x3*y4-y3*x4) / determinant
+                    #Check if intersection lies between the points
+                    dist_a_b = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+                    dist_a_p= math.sqrt((x1 - px)**2 + (y1 - py)**2)
+                    dist_b_p= math.sqrt((x2 - px)**2 + (y2 - py)**2)
+                    if(dist_a_p + dist_b_p ==dist_a_b ):
+                        print("There is an intersection at {},{}".format(px,py))
+                        assert False
+        pass
 
 
 def planar_drawing_conforms_to_embedding(embedding, pos):
