db56d19bac1a10e65c269e9af291be0bcfccc3df
Yassin Bahloul <yassin.bahloul@rwth-aachen.de>
2018-07-15 13:26:39 +0200

    Add _recursive version of lr_planarity.

--- networkx/algorithms/planarity.py
+++ networkx/algorithms/planarity.py
@@ -257,6 +257,50 @@ class LRPlanarity(object):
 
         return self.embedding
 
+    # recursive version of lr_planarity
+    def lr_planarity_recursive(self):
+        if self.G.order() > 2 and self.G.size() > 3 * self.G.order() - 6:
+            # graph is not planar
+            return None
+
+        # orientation of the graph by depth first search traversal
+        for v in self.G:
+            if self.height[v] is None:
+                self.height[v] = 0
+                self.roots.append(v)
+                self.dfs_orientation_recursive(v)
+
+        self.G = None  # just unsetting this for correctness purposes
+
+        # testing
+        for v in self.DG:  # sort the adjacency lists by nesting depth
+            # note: this sorting leads to non linear time
+            self.ordered_adjs[v] = sorted(
+                self.DG[v], key=lambda w: self.nesting_depth[(v, w)])
+        for v in self.roots:
+            if not self.dfs_testing_recursive(v):
+                return None
+
+        for e in self.DG.edges:
+            self.nesting_depth[e] = self.sign_recursive(e) * self.nesting_depth[e]
+
+        self.embedding.add_nodes_from(self.DG.nodes)
+        for v in self.DG:
+            # sort the adjacency lists again
+            self.ordered_adjs[v] = sorted(
+                self.DG[v], key=lambda w: self.nesting_depth[(v, w)])
+            # initialize the embedding
+            previous_node = None
+            for w in self.ordered_adjs[v]:
+                self.embedding.add_half_edge_cw(v, w, previous_node)
+                previous_node = w
+
+        # compute the complete embedding
+        for v in self.roots:
+            self.dfs_embedding_recursive(v)
+
+        return self.embedding
+
     # orient the graph by DFS-traversal, compute lowpoints and nesting order
     def dfs_orientation(self, v):
         # the recursion stack
