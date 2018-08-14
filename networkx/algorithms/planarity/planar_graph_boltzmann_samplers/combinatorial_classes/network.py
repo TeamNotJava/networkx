003975a4265df4f2836fdbe74dc9040620b062ff
Valkum <rudi.floren@gmail.com>
2018-08-14 18:18:28 +0200

    Fix network combinatorial class property error

--- networkx/algorithms/planarity/planar_graph_boltzmann_samplers/combinatorial_classes/network.py
+++ networkx/algorithms/planarity/planar_graph_boltzmann_samplers/combinatorial_classes/network.py
@@ -73,11 +73,11 @@ class Network(HalfEdgeGraph):
         return self._l_size
 
     def random_u_atom(self):
-        rand_index = rn.randrange(self.u_size()
+        rand_index = rn.randrange(self.u_size)
         return nth(self.u_atoms(), rand_index)
 
     def random_l_atom(self):
-        rand_index = rn.randrange(self.l_size())
+        rand_index = rn.randrange(self.l_size)
         return nth(self.l_atoms(), rand_index)
 
     def __str__(self):
